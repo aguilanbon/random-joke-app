f9630a8393154308adc1e7ccfccac8e9a83329c2
aguilanbon <aguilanbon@gmail.com>
2022-07-03 18:36:55 +0800

    added text state before fetching data

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 
-function Card({singleJoke, twoPartJoke}) {
+function Card({singleJoke, twoPartJoke, isEmpty}) {
   return (
     <div className='flex flex-col items-center justify-center bg-custom5 text-custom2 p-10 rounded-md shadow-2xl'>
-        {singleJoke ? <p>{singleJoke}</p> : <>
+        {singleJoke && <p>{singleJoke}</p>}
+
+        {twoPartJoke && <>
             <h2 className='w-full text-2xl text-center'>{twoPartJoke.setup}</h2>
             <p className='mt-8 font-bold'>{twoPartJoke.delivery}</p>
-        </>}
+        </>
+        }
+
+        {isEmpty && <p className='mb-8'>Press the button, I guess?</p> }
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,26 +9,28 @@ function Home() {
     
     const [singleJoke, setSingleJoke] = useState('')
     const [twoPartJoke, setTwoPartJoke] = useState({})
+    const [isEmpty, setIsEmpty] = useState(true)
 
     const getJoke = async () => {
         const response = await axios.get('https://v2.jokeapi.dev/joke/Any')
-        console.log(response.data);
         if (response.data.type === 'single') {
             setSingleJoke(response.data.joke)
             setTwoPartJoke('')
+            setIsEmpty(false)
         } else {
             setTwoPartJoke({
                 setup: response.data.setup,
                 delivery: response.data.delivery
             })
             setSingleJoke('')
+            setIsEmpty(false)
         }
     }
 
     return (
         <div className='flex flex-col items-center h-screen bg-gradient-to-tr from-custom3 to-custom4'>
             <div className='flex flex-col min-w-96 max-w-2xl mt-56'>
-                <Card singleJoke={singleJoke} twoPartJoke={twoPartJoke} />
+                <Card singleJoke={singleJoke} twoPartJoke={twoPartJoke} isEmpty={isEmpty} />
                 <Button getJoke={getJoke}/>
             </div>
         </div>
