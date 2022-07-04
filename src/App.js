aca8b0d07af888ed5c1c8c1ddfa7ca2d1802557b
aguilanbon <aguilanbon@gmail.com>
2022-07-04 12:41:08 +0800

    footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { GlobalProvider } from './helpers/GlobalContext';
 import './index.css';
 import Home from './pages/Home';
+import Footer from './components/Footer'
 
 function App() {
   return (
     <GlobalProvider>
       <div className="">
         <Home />
+        <Footer />
       </div>
     </GlobalProvider>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='flex w-full h-8 items-center justify-center bg-gray-800'>
+            <p className=' flex text-xs text-white '>Bon Aguilan | Front End Developer</p>
+        </div>
+    )
+}
+
+export default Footer
