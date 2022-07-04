817c36ca086e9f938ec2924d0b0b152c754ee149
aguilanbon <aguilanbon@gmail.com>
2022-07-04 14:33:19 +0800

    even margin on spacing - spacer component

--- src/components/Spacer.jsx
+++ src/components/Spacer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Spacer() {
     return (
-        <div className='flex mt-6'>
+        <div className='flex mt-8'>
             <div className='w-8 h-2 bg-gray-300 rounded-full'></div>
         </div>
     )
