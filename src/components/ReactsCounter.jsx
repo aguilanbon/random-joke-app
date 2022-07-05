09e360140dbcc403d9d542d483d67ae083896a2e
aguilanbon <aguilanbon@gmail.com>
2022-07-05 11:16:58 +0800

    center reacCounter component items

--- src/components/ReactsCounter.jsx
+++ src/components/ReactsCounter.jsx
@@ -8,11 +8,11 @@ function ReactsCounter() {
   return (
     <div className='flex flex-col items-center justify-center h-36 text-white '>
       <div className='flex flex-row mt-4 w-full items-center justify-around'>
-        <div className='w-12 flex flex-col'>
+        <div className='w-12 flex flex-col justify-center items-center'>
           <h1 className='text-3xl'>🤡</h1>
           <p>{thumbsDown}</p>
         </div>
-        <div className='w-12 flex flex-col'>
+        <div className='w-12 flex flex-col justify-center items-center'>
           <h1 className='text-3xl'>😂</h1>
           <p>{thumbsUp}</p>
         </div>
