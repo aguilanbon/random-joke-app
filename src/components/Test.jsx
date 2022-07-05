08044d1b5feee228c9fc129659633db13f14c01a
aguilanbon <aguilanbon@gmail.com>
2022-07-05 11:39:25 +0800

    initial files for custom modal

---
+++ src/components/Test.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Test() {
+    return (
+        <div className='flex bg-500 h-96 w-96'>
+        </div>
+    )
+}
+
+export default Test
---
+++ src/components/modal/Overlay.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Overlay() {
+    return (
+        <div className='bg-black-500 h-96 w-full'>
+            <h2 className='p-4 bg-red-500'>hahah</h2>
+        </div>
+    )
+}
+
+export default Overlay
