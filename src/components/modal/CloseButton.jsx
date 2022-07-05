03983fe6466ec4ca3cc1a883fc50a085f2b63227
aguilanbon <aguilanbon@gmail.com>
2022-07-05 15:12:21 +0800

    change modal x button hover state for readability

--- src/components/modal/CloseButton.jsx
+++ src/components/modal/CloseButton.jsx
@@ -7,7 +7,7 @@ function CloseButton() {
     const { setShowModal } = useContext(GlobalContext)
 
     return (
-        <motion.div whileHover={{ rotate: 360, scale: 1.1 }} onClick={() => setShowModal(false)} className='absolute h-6 w-6 rounded-full bg-red-300 text-white cursor-pointer flex flex-col content-center text-center items-center justify-center top-0 right-0 -mt-2 -mr-2'>
+        <motion.div whileHover={{ rotate: 360, scale: 1.1 }} onClick={() => setShowModal(false)} className='absolute h-6 w-6 rounded-full bg-red-300 hover:bg-red-500 text-white cursor-pointer flex flex-col content-center text-center items-center justify-center top-0 right-0 -mt-2 -mr-2'>
             <p className='h-auto text-xs'>x</p>
         </motion.div>
     )
