a20c850fddb064b04d61f6c466c3722c1dfea7a5
aguilanbon <aguilanbon@gmail.com>
2022-07-06 13:10:14 +0800

    modal data styling

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -8,9 +8,9 @@ function Modal() {
     const {liked} = useContext(GlobalContext)
 
     return (
-        <motion.div animate={{ scale: [0, 1.2, 1] }} exit={{ scale: [1, 1.1, 0] }} className='w-1/2 h-auto min-h-96 bg-custom5 z-50 rounded-lg p-8 relative mx-5'>
+        <motion.div animate={{ scale: [0, 1.2, 1] }} exit={{ scale: [1, 1.1, 0] }} className='w-1/2 h-auto bg-custom5 z-50 rounded-lg p-8 relative mx-5 mt-16'>
             <CloseButton />
-            <div className='flex flex-col'>
+            <div className='flex flex-col overflow-y-scroll max-h-96'>
                 <div className='flex w-full items-center justify-center mb-4'>
                     <h1 className='font-semibold'>Ok, so this made you laugh?.</h1>
                 </div>
@@ -18,13 +18,13 @@ function Modal() {
                     {liked.map(jokes => (
                         <div key={jokes.id}>
                             {jokes.type === 'single' ?
-                                <div className='text-custom2 my-4 border-l-2 border-custom2'>
+                                <div className='text-custom2 my-8 border-l-2 border-custom2'>
                                     <p className='ml-2'>{jokes.joke}</p>
                                 </div>
                             : 
-                                <div className='my-4 border-l-2 border-custom4'>
-                                    <p className='ml-2'>{jokes.setup}</p>
-                                    <p className='mt-2 ml-8 font-semibold'>{jokes.delivery}</p>
+                                <div className='my-8 border-l-2 border-custom4'>
+                                    <p className='ml-2 text-sm'>{jokes.setup}</p>
+                                    <p className='mt-2 ml-8 text-sm font-semibold'>{jokes.delivery}</p>
                                 </div>
                             }
                         </div>
--- src/components/modal/Overlay.jsx
+++ src/components/modal/Overlay.jsx
@@ -3,7 +3,7 @@ import Modal from './Modal'
 
 function Overlay() {
     return (
-        <div className='w-full h-full bg-black/[.50] absolute z-10 flex items-center justify-center'>
+        <div className='w-full h-full bg-black/[.50] absolute z-10 flex items-start justify-center'>
             <Modal />
         </div>
     )
